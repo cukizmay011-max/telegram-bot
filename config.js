2d83e8fb51bfa930618f2328c5f540bd2a1c07fa
cukizmay011-max <cukizmay011@gmail.com>
2026-06-28 15:09:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,10 @@
 module.exports = {
     TOKEN: process.env.TELEGRAM_TOKEN || "8894371867:AAGsyjEfc0QfkbNfHhv_jKtQibYBb2kTemc", // GANTI DENGAN TOKEN BOT ANDA
     MAIN_OWNER: "drzxcx", // GANTI DENGAN USERNAME OWNER (tanpa @)
-    MAIN_OWNER_ID: "8678912390",
+    OWNERS: [
+        "8678912390"
+    ],
+    
     MIN_MEMBER: 10,
     DUEL_DELAY: 5000,
     
