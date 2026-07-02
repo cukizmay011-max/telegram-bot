5337ac17ced28fa4c16d0b5b213fd6488a37e235
cukizmay011-max <cukizmay011@gmail.com>
2026-07-03 00:12:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,10 +2,11 @@ module.exports = {
     TOKEN: process.env.TELEGRAM_TOKEN || "8894371867:AAGsyjEfc0QfkbNfHhv_jKtQibYBb2kTemc", // GANTI DENGAN TOKEN BOT ANDA
     MAIN_OWNER: "drazxreal", // GANTI DENGAN USERNAME OWNER (tanpa @)
     OWNERS: [
-        "8622543593"
+        "8622543593,
+         8678912390"
     ],
     
-    MIN_MEMBER: 10,
+    MIN_MEMBER: 100,
     DUEL_DELAY: 5000,
     
     MENU_IMAGE:"https://files.catbox.moe/l12ea3.png"
